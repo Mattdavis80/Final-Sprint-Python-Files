9e59798ca7bf90a639a890932b642f64fa7d2b5c
Mike93 <127118338+Mike93@users.noreply.github.com>
2023-04-06 15:40:01 -0230

    Update testingcust.py
    Updated file with option four function

--- testingcust.py
+++ testingcust.py
@@ -1,15 +1,19 @@
 import re
 import datetime
 
+today = datetime.datetime.now()
+today_str = datetime.datetime.strftime(today, "%m/%d/%Y")
+
 valid_prov = ["NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC", "YT", "NT", "NU"]
 # Read data from the file defaults.dat
 f = open('defaults.dat', 'r')
-employee_num = f.readline()
-inventory_num = f.readline()
-commission_rate = f.readline()
-bonus_threshold = f.readline()
-commission_bonus_amt = f.readline()
-reorder_num = f.readline()
+employee_num = int(f.readline())
+inventory_num = int(f.readline())
+commission_rate = float(f.readline())
+bonus_threshold = float(f.readline())
+commission_bonus_amt = float(f.readline())
+reorder_num = int(f.readline())
+HST = float(f.readline())
 f.close()
 
 def option_one():
@@ -99,7 +103,7 @@ def option_one():
             print("You make fucking mistake in the date of hiring, common")
 
     employee_info.append((employee_num.strip(), emp_f_name, emp_l_name, str_add, city, prov, post_code, phone_num))
-
+    employee_info[0] += 1
     f = open('employeeLog.dat', 'a')
     for data in employee_info:
         f.write(",".join(map(str, data)) + "\n")
@@ -109,6 +113,8 @@ def option_one():
 
 
 def option_two():
+    customer_info = []
+
     pass
 
 
@@ -116,7 +122,148 @@ def option_three():
     pass
 
 
+item_file = open('inventoryLog.dat', 'r')
+
+item_numbers_list = []
+item_descriptions_list = []
+for item_data_line in item_file:
+    item_line = item_data_line.split(',')
+
+    item_number = int(item_line[0].strip())
+    item_description = item_line[1].strip()
+    retail_price = item_line[7].strip()
+
+    item_numbers_list.append(item_number)
+    item_descriptions_list.append(item_description)
+
+
+item_file.close()
+
+employee_file = open("employeeLog.dat", "r")
+
+emp_numbers_list = []
+
+for employee_data_line in employee_file:
+    employee_line = employee_data_line.split(',')
+
+    employee_num = employee_line[0].strip()
+
+    emp_numbers_list.append(employee_num)
+
+employee_file.close()
+
+f = open("customerLog.dat", "r")
+
+customer_numbers_list = []
+for customer_data_line in f:
+    customer_line = customer_data_line.split(',')
+
+    customer_number = int(customer_line[0].strip())
+
+    customer_numbers_list.append(customer_number)
+
+f.close()
+
+purchase_file = open("customerPurchase.dat", "r")
+
+for purchase_data_line in purchase_file:
+    purchase_line = purchase_data_line.split(',')
+
+    order_number = int(purchase_line[0].strip())
+
+purchase_file.close()
+
+
 def option_four():
+    purchase_info = []
+    print("Enter new order information")
+
+    while True:
+        emp_number = input("Employee Number: ")
+        if emp_number == "":
+            print("Cannot be blank - please try again.")
+        elif emp_number not in emp_numbers_list:
+            print("Does not match any current employee number - please try again.")
+        else:
+            break
+
+    while True:
+        # our customerLog file contains customer numbers of 4000 - 4005
+        # so this section should only work if entering 4000 - 4005
+        customer_num = int(input("Customer Number: "))
+        if customer_num not in customer_numbers_list:
+            new_cust_msg = input("Customer does not exist - would you like to add a new customer? (Y/N): ").upper()
+            if new_cust_msg != "Y" and new_cust_msg != "N":
+                print("Invalid input - please enter 'Y' or 'N'")
+            elif new_cust_msg == "Y":
+                option_two()
+                # of course this won't actually add a new customer because we only had to
+                # do option 1 or 2 or 3 and option 1 is done, so I didn't actually code option_two()
+                # Let's pretend this actually added a new customer then get sent back up
+                # to ask for customer number again, where we can enter 4000 - 4005 to continue
+            else:
+                continue
+        else:
+            break
+
+    while True:
+        try:
+            item_num = int(input("Item Number: "))
+        except ValueError:
+            print("Invalid input - must enter an integer.")
+        else:
+            if item_num not in item_numbers_list:
+                print("That item number does not match any of our items - please try again.")
+            else:
+                break
+
+    while True:
+        description = input("Item Description: ")
+        if description == "":
+            print("Cannot be blank - please try again.")
+        elif description not in item_descriptions_list:
+            print("We do not sell any items of that description - please try again.")
+            print("Here's a list of valid descriptions: ")
+            print("'Wool Carpet', 'Nylon Carpet', 'Blended Carpet'")
+        else:
+            break
+
+    while True:
+        try:
+            retail_cost = float(input("Enter the retail cost: "))
+        except ValueError:
+            print("Invalid number - please try again")
+        else:
+            break
+
+    while True:
+        try:
+            quantity = int(input("Enter the quantity purchased: "))
+        except ValueError:
+            print("Invalid number - please try again")
+        else:
+            if quantity < 1:
+                print("Invalid quantity - please try again")
+            else:
+                break
+
+    # calculations for customer purchase
+    subtotal = retail_cost * quantity
+    HST_amount = subtotal * HST
+    order_total = subtotal + HST_amount
+
+    order_date = today_str
+
+    # Put all purchase info into list
+    purchase_info.append((order_number, order_date, customer_num, item_num, description, retail_cost, quantity, subtotal, order_total, emp_number))
+    purchase_info[0] += 1   # Increment order_number before writing it back
+    # Open file and write back all data
+    order_file = open("customerPurchase.dat", "a")
+
+    for data in purchase_info:
+        order_file.write(",".join(map(str, data)) + "\n")
+    order_file.close()
+
     pass
 
 
